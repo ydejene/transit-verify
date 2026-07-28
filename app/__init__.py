5fd40f4b47bcb8064c017857d4f237def05454aa
ydejene <y.dejene@alustudent.com>
2026-07-28 12:46:22 +0300

    feat: add modular CSS, base template, and JS

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,13 @@
 import os
 from flask import Flask
-from app.routes import auth, dashboard, mobile, ussd, simple
+
+# from app.routes import auth, dashboard, mobile, ussd, simple
+from app.routes import simple
+
 
 def create_app():
-    app = Flask(__name__, static_folder='static', template_folder='templates')
-    app.config.from_pyfile(os.path.join(app.instance_path, 'config.py'), silent=True)
+    app = Flask(__name__, static_folder="static", template_folder="templates")
+    app.config.from_pyfile(os.path.join(app.instance_path, "config.py"), silent=True)
     # app.register_blueprint(auth.bp)
     # app.register_blueprint(dashboard.bp)
     # app.register_blueprint(mobile.bp)
