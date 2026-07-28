46da63c5d5b24802b255e42aa2c3539e19371fbd
ydejene <y.dejene@alustudent.com>
2026-07-28 15:32:15 +0300

    feat: add modular dashboard.css and Jinja2 dashboard template

--- app/routes/simple.py
+++ app/routes/simple.py
@@ -5,3 +5,7 @@ bp = Blueprint('simple', __name__)
 @bp.route('/')
 def home():
     return render_template('pages/login.html')
+
+@bp.route('/dashboard')
+def dashboard():
+    return render_template('pages/dashboard.html')
