4a4feb7d2881efeca592f10ee1e6402bba765a7c
ydejene <y.dejene@alustudent.com>
2026-07-28 14:02:21 +0300

    fix: adjust formatting and add flake8 ignores

--- app/routes/simple.py
+++ app/routes/simple.py
@@ -1,7 +1,8 @@
 from flask import Blueprint, render_template
 
-bp = Blueprint('simple', __name__)
+bp = Blueprint("simple", __name__)
 
-@bp.route('/')
+
+@bp.route("/")
 def home():
-    return render_template('pages/login.html')
+    return render_template("pages/login.html")
