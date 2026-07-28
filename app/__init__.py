0a0e8d64d3743588ae4c5f57b3ab2abd8689eddb
ydejene <y.dejene@alustudent.com>
2026-07-28 11:54:20 +0300

    Add simple home route rendering login template

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,13 @@
 import os
 from flask import Flask
-from app.routes import auth, dashboard, mobile, ussd
+from app.routes import auth, dashboard, mobile, ussd, simple
 
 def create_app():
     app = Flask(__name__, static_folder='static', template_folder='templates')
     app.config.from_pyfile(os.path.join(app.instance_path, 'config.py'), silent=True)
-    app.register_blueprint(auth.bp)
-    app.register_blueprint(dashboard.bp)
-    app.register_blueprint(mobile.bp)
-    app.register_blueprint(ussd.bp)
+    # app.register_blueprint(auth.bp)
+    # app.register_blueprint(dashboard.bp)
+    # app.register_blueprint(mobile.bp)
+    # app.register_blueprint(ussd.bp)
+    app.register_blueprint(simple.bp)
     return app
